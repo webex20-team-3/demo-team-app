c19b7f27167bd810c828c764880a5994009a8589
kusabakosuke <kousukekusaba@gmail.com>
2023-11-28 16:13:51 +0900

    Paletteを作成しました

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -1,13 +1,63 @@
 <template>
   <h1>Vue パレット</h1>
   <div class="app">
-    <div class="palette" style="background-color: rgba(0, 0, 200, 0.5)"></div>
-    <p>rgba( {{ 0 }}, {{ 0 }}, 200, 0.5 )</p>
+    <div
+      class="palette"
+      style="background-color: rgba(0, 0, 200, 0.5)"
+      v-on:mousemove="changeColor"
+      v-on:click="pickColor"
+      v-bind:style="paletteStyle"
+    ></div>
+    <p>rgba( {{ red }}, {{ green }}, 200, 0.5 )</p>
     <div class="colors-container">
-      <div class="mini-palette"></div>
+      <div
+        class="mini-palette"
+        v-for="color in colors"
+        :key="color"
+        v-bind:style="{
+          backgroundColor: `rgba(${color.red}, ${color.green}, 200, 0.5)`,
+        }"
+        v-on:click="showColor(color)"
+      ></div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      red: 0,
+      green: 0,
+      colors: [],
+    }
+  },
+  methods: {
+    changeColor(e) {
+      this.red = e.offsetX
+      this.green = e.offsetX
+    },
+    pinkColor() {
+      const newColor = {
+        red: this.red,
+        green: this.green,
+      }
+      this.colors.push(newColor)
+    },
+    showColor(color) {
+      this.red = color.red
+      this.green = color.green
+    },
+  },
+  computed: {
+    paletteStyle() {
+      return {
+        backgroundColor: `rgba(${this.red}, ${this.green}, 200, 0.5)`,
+      }
+    },
+  },
+}
+</script>
 
 <style>
 .app {
